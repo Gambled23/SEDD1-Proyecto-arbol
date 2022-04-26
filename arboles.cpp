4dce88e85878bea058b4e86224dc14c279952572
Cesar <ipog71@gmail.com>
2022-04-26 11:37:42 -0500

    Menu completado (faltan funciones)

--- arboles.cpp
+++ arboles.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include "validaciones.hpp"
+#include "nodo.hpp"
+#include "contacto.hpp"
 
+contacto contactoAux;
 void menu();
 using namespace std;
 int main()
@@ -13,35 +17,47 @@ void menu()
 {
     char respuestaChar[100];
     int respuestaInt = 0;
+    int auxID;
+    string auxNombre;
     do
     {
         system("cls");
         cout << "Bienvenido al programa para arboles\n";
-        cout << "1) Insertar datos\n";
-        cout << "2) Buscar nodo por ID\n";
-        cout << "3) Buscar nodo por NOMBRE\n";
-        cout << "4) Mostrar minimo ID\n";
-        cout << "5) Mostrar maximo ID\n";
-        cout << "6) Mostrar nodo antecesor\n";
-        cout << "7) Mostrar nodo sucesor\n";
-        cout << "8) Editar nodo (por ID)\n";
-        cout << "9) Eliminar nodo (por ID)\n";
-        cout << "10) Mostrar datos en orden\n";
-        cout << "11) Mostrar datos en preorden\n";
-        cout << "12) Mostrar datos en postorden\n";
+        cout << "1) Insertar datos\n";              // Todo llamada a funcion
+        cout << "2) Buscar nodo por ID\n";          // Todo
+        cout << "3) Buscar nodo por NOMBRE\n";      // Todo
+        cout << "4) Mostrar minimo ID\n";           // Todo
+        cout << "5) Mostrar maximo ID\n";           // Todo
+        cout << "6) Mostrar nodo antecesor\n";      // Todo
+        cout << "7) Mostrar nodo sucesor\n";        // Todo
+        cout << "8) Editar nodo (por ID)\n";        // Todo
+        cout << "9) Eliminar nodo (por ID)\n";      // Todo
+        cout << "10) Mostrar datos en orden\n";     // Todo
+        cout << "11) Mostrar datos en preorden\n";  // Todo
+        cout << "12) Mostrar datos en postorden\n"; // Todo
         cout << "\n0) Salir\n";
         cin >> respuestaChar;
         respuestaInt = validarNumInt(respuestaChar);
         switch (respuestaInt)
         {
         case 1:
-            /* code */
+            cout << "Ingrese el nombre del contacto\n";
+            cin.sync();
+            getline(cin, contactoAux.nombre);
+            cout << "Ingrese el email de " << contactoAux.nombre << endl;
+            cin.sync();
+            getline(cin, contactoAux.email);
+            cout << "Ingrese la direccion de " << contactoAux.nombre << endl;
+            cin.sync();
+            getline(cin, contactoAux.direccion);
             break;
         case 2:
-            /* code */
+            cout<<"Ingrese la ID del contacto a buscar\n";
+            cin>>auxID;
             break;
         case 3:
-            /* code */
+            cout<<"Ingrese el nombre del contacto a buscar\n";
+            cin>>auxNombre;
             break;
         case 4:
             /* code */
@@ -56,10 +72,12 @@ void menu()
             /* code */
             break;
         case 8:
-            /* code */
+            cout<<"Ingrese la ID del contacto a editar\n";
+            cin>>auxID;
             break;
         case 9:
-            /* code */
+            cout<<"Ingrese la ID del contacto a eliminar\n";
+            cin>>auxID;
             break;
         case 10:
             /* code */
