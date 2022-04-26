fe8c884a6504e2c4fee84dc12c0b08b5e8e826ae
Cesar <ipog71@gmail.com>
2022-04-26 11:44:15 -0500

    Arbol esqueleto

---
+++ arbol.hpp
@@ -0,0 +1,77 @@
+#ifndef ARBOL_H
+#define ARBOL_H
+#include <iostream>
+#include <cstdlib>
+#include "nodo.hpp"
+#include "validaciones.hpp"
+#include "contacto.hpp"
+#pragma once
+
+using namespace std;
+
+class arbol
+{
+public:
+    arbol();
+    nodo *raiz;
+    nodo *actual;
+    void inicializa();
+    void insertar(contacto);
+    void eliminar(int);
+    void editar(int);
+    void buscar(int);
+    void mostrar();
+    void orden();
+    void preorden();
+    void postorden();
+    void antecesor();
+    void sucesor();
+    void minimo();
+    void maximo();
+
+private:
+};
+
+arbol::arbol()
+{
+}
+void arbol::inicializa()
+{
+}
+void arbol::insertar(contacto e)
+{
+}
+void arbol::eliminar(int n)
+{
+}
+void arbol::editar(int n)
+{
+}
+void arbol::buscar(int n)
+{
+}
+void arbol::mostrar()
+{
+}
+void arbol::orden()
+{
+}
+void arbol::preorden()
+{
+}
+void arbol::postorden()
+{
+}
+void arbol::antecesor()
+{
+}
+void arbol::sucesor()
+{
+}
+void arbol::minimo()
+{
+}
+void arbol::maximo()
+{
+}
+#endif
