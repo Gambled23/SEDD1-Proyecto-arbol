0f7c72b0315fb6b242868f89565a802d41507839
Cesar <ipog71@gmail.com>
2022-04-26 11:23:12 -0500

    Menu

--- arboles.cpp
+++ arboles.cpp
@@ -12,20 +12,71 @@ int main()
 void menu()
 {
     char respuestaChar[100];
-    int respuestaInt;
-    cout<<"Bienvenido al programa para arboles\n";
-    cout<<"1) Insertar datos\n";
-    cout<<"2) Buscar nodo por ID\n";
-    cout<<"3) Buscar nodo por NOMBRE\n";
-    cout<<"4) Mostrar minimo ID\n";
-    cout<<"5) Mostrar maximo ID\n";
-    cout<<"6) Mostrar nodo antecesor\n";
-    cout<<"7) Mostrar nodo sucesor\n";
-    cout<<"8) Editar nodo (por ID)\n";
-    cout<<"9) Eliminar nodo (por ID)\n";
-    cout<<"10) Mostrar datos en orden\n";
-    cout<<"11) Mostrar datos en preorden\n";
-    cout<<"12) Mostrar datos en postorden\n";
-    cin>>respuestaChar;
-    respuestaInt = validarNumInt(respuestaChar);
+    int respuestaInt = 0;
+    do
+    {
+        system("cls");
+        cout << "Bienvenido al programa para arboles\n";
+        cout << "1) Insertar datos\n";
+        cout << "2) Buscar nodo por ID\n";
+        cout << "3) Buscar nodo por NOMBRE\n";
+        cout << "4) Mostrar minimo ID\n";
+        cout << "5) Mostrar maximo ID\n";
+        cout << "6) Mostrar nodo antecesor\n";
+        cout << "7) Mostrar nodo sucesor\n";
+        cout << "8) Editar nodo (por ID)\n";
+        cout << "9) Eliminar nodo (por ID)\n";
+        cout << "10) Mostrar datos en orden\n";
+        cout << "11) Mostrar datos en preorden\n";
+        cout << "12) Mostrar datos en postorden\n";
+        cout << "\n0) Salir\n";
+        cin >> respuestaChar;
+        respuestaInt = validarNumInt(respuestaChar);
+        switch (respuestaInt)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        case 6:
+            /* code */
+            break;
+        case 7:
+            /* code */
+            break;
+        case 8:
+            /* code */
+            break;
+        case 9:
+            /* code */
+            break;
+        case 10:
+            /* code */
+            break;
+        case 11:
+            /* code */
+            break;
+        case 12:
+            /* code */
+            break;
+        case 0:
+            cout << "Gracias por su preferencia\n";
+            break;
+        default:
+            cout << "Respuesta no valida\n";
+            break;
+        }
+        system("PAUSE");
+    } while (respuestaInt != 0);
 }
